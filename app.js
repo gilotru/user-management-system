1adfc5ec2a79e4a6d86389de0bba954f5d462fc2
gilotru <delacruzangelo686@gmail.com>
2022-05-22 09:01:49 +0800

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const Connection = require('mysql/lib/Connection');
 
 require('dotenv').config();
 
@@ -19,12 +20,27 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 //Templating engine
-app.engine('hbs', exphbs( {extname: '.hbs' }));
+const handlebars = exphbs.create({extname: '.hbs'});
+app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
+
+//connection pool
+const pool = mysql.createPool({
+    connectionLimit : 100,
+    host            : process.env.DB_HOST,
+    user            : process.env.DB_USER,
+    password        : process.env.DB_PASS,
+    database        : process.env.DB_NAME
+});
+
+pool.getConnection((err, Connection) =>{
+ if (err) throw err; //not connected
+ console.log('connected as ID', Connection.threadId)
+});
 
 //routes
 app.get('', (req, res) =>{
     res.render('home');
-})
+});
 
 app.listen(port, () => console.log(`listening on port ${port}`));
