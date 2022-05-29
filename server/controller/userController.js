63218630a73b19a21cfbe7712ffb6d1738e41c01
gilotru <delacruzangelo686@gmail.com>
2022-05-29 14:13:08 +0800

    add view user

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -20,6 +20,7 @@ pool.getConnection((err, Connection) =>{
      Connection.release();
 
      if(!err){
+         let removedUser = re.query.removed;
          res.render('home', {rows});
      }else{
          console.log(err);
@@ -74,5 +75,92 @@ exports.create = (req, res)=>{
 
 // edit user
 exports.edit = (req, res)=>{
-    res.render('edit-user');
+//DB connection
+    pool.getConnection((err, Connection) =>{
+        if (err) throw err; //not connected
+        console.log('connected as ID', Connection.threadId);
+        Connection.query('SELECT * FROM user_table WHERE id = ?', [req.params.id], (err, rows)=>{
+            //when connection is done
+            Connection.release();
+    
+            if(!err){
+                res.render('edit-user', {rows});
+            }else{
+                console.log(err);
+            }
+        });
+    });
 }
+
+// update user
+exports.update = (req, res)=>{
+const {first_name, last_name, email, phone_number, comment} = req.body;
+//DB connection
+    pool.getConnection((err, Connection) =>{
+        if (err) throw err; //not connected
+        console.log('connected as ID', Connection.threadId);
+        Connection.query('UPDATE user_table SET first_name = ?, last_name = ?, email = ?, phone_number = ?, comment = ?, WHERE id = ?', [first_name, last_name, email, phone_number, comment, req.params.id], (err, rows)=>{
+            //when connection is done
+            Connection.release();
+    
+            if(!err){
+                pool.getConnection((err, Connection) =>{
+                    if (err) throw err; //not connected
+                    console.log('connected as ID', Connection.threadId);
+                    Connection.query('SELECT * FROM user_table WHERE id = ?', [req.params.id], (err, rows)=>{
+                        //when connection is done
+                        Connection.release();
+                
+                        if(!err){
+                            res.render('edit-user', {rows, alert: `${first_name} has been updated.`});
+                        }else{
+                            console.log(err);
+                        }
+                    });
+                });
+
+            }else{
+                console.log(err);
+            }
+        });
+    });
+}
+
+// delete user
+exports.delete = (req, res)=>{
+//DB connection
+    pool.getConnection((err, Connection) =>{
+        if (err) throw err; //not connected
+        console.log('connected as ID', Connection.threadId);
+        Connection.query('DELETE FROM user_table SET status = ? WHERE id = ?', ['removed', req.params.id], (err, rows)=>{
+            //when connection is done
+            Connection.release();
+
+            if(!err){
+                let removedUser = encodeURIComponent('User successefully removed');
+                res.redirect('/?removed=' + removedUser);
+            }else{
+                console.log(err);
+            }
+        });
+    });
+}
+
+// view all user
+exports.viewall = (req, res)=>{
+    //DB connection
+        pool.getConnection((err, Connection) =>{
+            if (err) throw err; //not connected
+            console.log('connected as ID', Connection.threadId);
+            Connection.query('SELECT * FROM user_table WHERE id = ?', [req.params.id], (err, rows)=>{
+                //when connection is done
+                Connection.release();
+        
+                if(!err){
+                    res.render('view-user', {rows});
+                }else{
+                    console.log(err);
+                }
+            });
+        });
+    }
--- server/route/user.js
+++ server/route/user.js
@@ -8,4 +8,7 @@ router.post('/', userController.find);
 router.get('/adduser', userController.form);
 router.post('/adduser', userController.create);
 router.get('/edituser/:id', userController.edit);
+router.post('/edituser/:id', userController.update);
+router.get('/viewuser/:id', userController.viewall);
+router.get('/:id', userController.delete);
 module.exports = router;
